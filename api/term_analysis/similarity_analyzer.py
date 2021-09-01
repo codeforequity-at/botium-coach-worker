770a99d59faac25ea1c8b2ffbc9dad966d3fa491
Paul Pröll <paul.proell@hotmail.com>
2021-09-01 22:12:14 +0200

    1000 threads

--- api/term_analysis/similarity_analyzer.py
+++ api/term_analysis/similarity_analyzer.py
@@ -3,24 +3,22 @@ import pandas as pd
 
 from sklearn.feature_extraction.text import CountVectorizer
 from concurrent.futures import ThreadPoolExecutor, as_completed
-import multiprocessing as mp
-from functools import partial
 
-def pd_frame(ns, obj):
-    #workspace_pd = obj["workspace_pd"]
+def pd_frame(obj):
+    workspace_pd = obj["workspace_pd"]
     index = obj["index"]
     logger = obj["logger"]
-    #cos_sim_score_matrix = obj["cos_sim_score_matrix"]
+    cos_sim_score_matrix = obj["cos_sim_score_matrix"]
     if (
-        ns.workspace_pd["intent"].iloc[index[0]]
-        != ns.workspace_pd["intent"].iloc[index[1]]
+        workspace_pd["intent"].iloc[index[0]]
+        != workspace_pd["intent"].iloc[index[1]]
     ):
         logger.info('Index started %s', index)
-        intent1 = ns.workspace_pd["intent"].iloc[index[0]]
-        utterance1 = ns.workspace_pd["utterance"].iloc[index[0]]
-        intent2 = ns.workspace_pd["intent"].iloc[index[1]]
-        utterance2 = ns.workspace_pd["utterance"].iloc[index[1]]
-        score = ns.cos_sim_score_matrix[index[0], index[1]]
+        intent1 = workspace_pd["intent"].iloc[index[0]]
+        utterance1 = workspace_pd["utterance"].iloc[index[0]]
+        intent2 = workspace_pd["intent"].iloc[index[1]]
+        utterance2 = workspace_pd["utterance"].iloc[index[1]]
+        score = cos_sim_score_matrix[index[0], index[1]]
         temp_pd = pd.DataFrame(
             {
                 "name1": [intent1],
@@ -62,20 +60,16 @@ def ambiguous_examples_analysis(logger, workspace_pd, threshold=0.7):
 
     logger.info('chi2 similarity: post processing')
     task_data = []
-    #executer = ThreadPoolExecutor(max_workers = 100)
-    mgr = mp.Manager()
-    ns = mgr.Namespace()
-    ns.workspace_pd = workspace_pd
-    ns.cos_sim_score_matrix = cos_sim_score_matrix
-    pool = mp.Pool(5)
+    executer = ThreadPoolExecutor(max_workers = 1000)
     for index in similar_utterance_index:
         logger.info('index %s of %s', index, len(similar_utterance_index))
-        task_data.append({
+        task_data.append(executer.submit(pd_frame, {
+            "workspace_pd": workspace_pd,
             "index": index,
+            "cos_sim_score_matrix": cos_sim_score_matrix,
             "logger": logger
-        })
-    #temp_pds = as_completed(task_data)#executer.map(pd_frame, tuple(task_data))
-    temp_pds = pool.imap_unordered(partial(pd_frame, ns), task_data)
+        }))
+    temp_pds = as_completed(task_data)#executer.map(pd_frame, tuple(task_data))
     for temp_pd in temp_pds:
         if temp_pd is not None:
             similar_utterance_pd = similar_utterance_pd.append(
