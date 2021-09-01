6d5a9619a353b9728ccadc27e936ffd513920c65
Paul Pröll <paul.proell@hotmail.com>
2021-09-01 23:15:37 +0200

    commented out pandas

--- api/term_analysis/similarity_analyzer.py
+++ api/term_analysis/similarity_analyzer.py
@@ -55,10 +55,10 @@ def ambiguous_examples_analysis(logger, workspace_pd, threshold=0.7):
             != workspace_pd["intent"].iloc[index[1]]
         ):
             logger.info('Index started %s', index)
-            intent1 = workspace_pd["intent"].iloc[index[0]]
-            utterance1 = workspace_pd["utterance"].iloc[index[0]]
-            intent2 = workspace_pd["intent"].iloc[index[1]]
-            utterance2 = workspace_pd["utterance"].iloc[index[1]]
+            #intent1 = workspace_pd["intent"].iloc[index[0]]
+            #utterance1 = workspace_pd["utterance"].iloc[index[0]]
+            #intent2 = workspace_pd["intent"].iloc[index[1]]
+            #utterance2 = workspace_pd["utterance"].iloc[index[1]]
             score = cos_sim_score_matrix[index[0], index[1]]
             temp_pd = pd.DataFrame(
                 {
