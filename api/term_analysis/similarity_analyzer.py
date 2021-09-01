b27933b264c797ed3e321f78d4cb7e2b084657d9
Paul Pröll <paul.proell@hotmail.com>
2021-09-01 11:55:01 +0200

    added comments in cosine chi2 similarity

--- api/term_analysis/similarity_analyzer.py
+++ api/term_analysis/similarity_analyzer.py
@@ -18,7 +18,7 @@ def ambiguous_examples_analysis(logger, workspace_pd, threshold=0.7):
     workspace_bow = vectorizer.fit_transform(workspace_pd["utterance"]).todense()
 
     logger.info('chi2 similarity: calculate_cosine_similarity')
-    cos_sim_score_matrix = _calculate_cosine_similarity(workspace_bow)
+    cos_sim_score_matrix = _calculate_cosine_similarity(logger, workspace_bow)
 
     logger.info('chi2 similarity: remove the lower triangle of the matrix and apply threshold')
     # remove the lower triangle of the matrix and apply threshold
@@ -56,13 +56,16 @@ def ambiguous_examples_analysis(logger, workspace_pd, threshold=0.7):
     similarity_df_sorted = similar_utterance_pd.sort_values(by=["similarity"], ascending=False)
     return [ { 'name1': name1, 'example1': example1, 'name2': name2, 'example2': example2, 'similarity': similarity } for name1, example1, name2, example2, similarity in zip(similarity_df_sorted['name1'], similarity_df_sorted['example1'], similarity_df_sorted['name2'], similarity_df_sorted['example2'], similarity_df_sorted['similarity'])]
 
-def _calculate_cosine_similarity(workspace_bow):
+def _calculate_cosine_similarity(logger, workspace_bow):
     """
     Given bow representation of the workspace utterance, calculate cosine similarity score
     :param workspace_bow: dense representation of BOW of workspace utterances
     :return: cosine_similarity_matrix
     """
     # normalized and calculate cosine similarity
+    logger.info('1')
     workspace_bow = workspace_bow / np.linalg.norm(workspace_bow, axis=1, keepdims=True)
+    logger.info('2')
     cosine_similarity_matrix = workspace_bow.dot(np.transpose(workspace_bow))
+    logger.info('3')
     return cosine_similarity_matrix
