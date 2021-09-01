b9f6220486bbcfebc5cc2ffac091bce4bf9e1975
Paul Pröll <paul.proell@hotmail.com>
2021-09-01 16:58:23 +0200

    added async map

--- api/term_analysis/similarity_analyzer.py
+++ api/term_analysis/similarity_analyzer.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.feature_extraction.text import CountVectorizer
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def pd_frame(obj):
     workspace_pd = obj["workspace_pd"]
@@ -60,16 +60,16 @@ def ambiguous_examples_analysis(logger, workspace_pd, threshold=0.7):
 
     logger.info('chi2 similarity: post processing')
     task_data = []
+    executer = ThreadPoolExecutor(max_workers = 10)
     for index in similar_utterance_index:
         logger.info('index %s of %s', index, len(similar_utterance_index))
-        task_data.append({
+        task_data.append(executer.submit(pd_frame, {
             "workspace_pd": workspace_pd,
             "index": index,
             "cos_sim_score_matrix": cos_sim_score_matrix,
             "logger": logger
-        })
-    executer = ThreadPoolExecutor(max_workers = 10)
-    temp_pds = executer.map(pd_frame, tuple(task_data))
+        }))
+    temp_pds = as_completed(task_data)#executer.map(pd_frame, tuple(task_data))
     for temp_pd in temp_pds:
         if temp_pd is not None:
             similar_utterance_pd = similar_utterance_pd.append(
