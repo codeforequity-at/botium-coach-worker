1f91b61a7ef92d3dad08abe1bcab38c1735bbf13
Paul Pröll <paul.proell@hotmail.com>
2021-09-01 21:35:03 +0200

    ns manager

--- api/term_analysis/similarity_analyzer.py
+++ api/term_analysis/similarity_analyzer.py
@@ -4,6 +4,35 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import multiprocessing as mp
+from functools import partial
+
+def pd_frame(ns, obj):
+    #workspace_pd = obj["workspace_pd"]
+    index = obj["index"]
+    logger = obj["logger"]
+    #cos_sim_score_matrix = obj["cos_sim_score_matrix"]
+    if (
+        ns.workspace_pd["intent"].iloc[index[0]]
+        != ns.workspace_pd["intent"].iloc[index[1]]
+    ):
+        logger.info('Index started %s', index)
+        intent1 = ns.workspace_pd["intent"].iloc[index[0]]
+        utterance1 = ns.workspace_pd["utterance"].iloc[index[0]]
+        intent2 = ns.workspace_pd["intent"].iloc[index[1]]
+        utterance2 = ns.workspace_pd["utterance"].iloc[index[1]]
+        score = ns.cos_sim_score_matrix[index[0], index[1]]
+        temp_pd = pd.DataFrame(
+            {
+                "name1": [intent1],
+                "example1": [utterance1],
+                "name2": [intent2],
+                "example2": [utterance2],
+                "similarity": [score],
+            }
+        )
+        logger.info('Index done %s', index)
+        return temp_pd
+    return None
 
 def ambiguous_examples_analysis(logger, workspace_pd, threshold=0.7):
     """
@@ -34,19 +63,19 @@ def ambiguous_examples_analysis(logger, workspace_pd, threshold=0.7):
     logger.info('chi2 similarity: post processing')
     task_data = []
     #executer = ThreadPoolExecutor(max_workers = 100)
+    mgr = mp.Manager()
+    ns = mgr.Namespace()
     pool = mp.Pool(5)
+    ns.workspace_pd = workspace_pd
+    ns.cos_sim_score_matrix = cos_sim_score_matrix
     for index in similar_utterance_index:
         logger.info('index %s of %s', index, len(similar_utterance_index))
         task_data.append({
             "index": index,
             "logger": logger
         })
-    global global_workspace_pd
-    global global_cos_sim_score_matrix
-    global_workspace_pd = workspace_pd
-    global_cos_sim_score_matrix = cos_sim_score_matrix
     #temp_pds = as_completed(task_data)#executer.map(pd_frame, tuple(task_data))
-    temp_pds = pool.imap_unordered(pd_frame, task_data)
+    temp_pds = pool.imap_unordered(partial(pd_frame, ns), task_data)
     for temp_pd in temp_pds:
         if temp_pd is not None:
             similar_utterance_pd = similar_utterance_pd.append(
@@ -56,34 +85,6 @@ def ambiguous_examples_analysis(logger, workspace_pd, threshold=0.7):
     logger.info('chi2 similarity: sorting by similarity')
     similarity_df_sorted = similar_utterance_pd.sort_values(by=["similarity"], ascending=False)
     return [ { 'name1': name1, 'example1': example1, 'name2': name2, 'example2': example2, 'similarity': similarity } for name1, example1, name2, example2, similarity in zip(similarity_df_sorted['name1'], similarity_df_sorted['example1'], similarity_df_sorted['name2'], similarity_df_sorted['example2'], similarity_df_sorted['similarity'])]
-
-def pd_frame(obj):
-    #workspace_pd = obj["workspace_pd"]
-    index = obj["index"]
-    logger = obj["logger"]
-    #cos_sim_score_matrix = obj["cos_sim_score_matrix"]
-    if (
-        global_workspace_pd["intent"].iloc[index[0]]
-        != global_workspace_pd["intent"].iloc[index[1]]
-    ):
-        logger.info('Index started %s', index)
-        intent1 = global_workspace_pd["intent"].iloc[index[0]]
-        utterance1 = global_workspace_pd["utterance"].iloc[index[0]]
-        intent2 = global_workspace_pd["intent"].iloc[index[1]]
-        utterance2 = global_workspace_pd["utterance"].iloc[index[1]]
-        score = global_cos_sim_score_matrix[index[0], index[1]]
-        temp_pd = pd.DataFrame(
-            {
-                "name1": [intent1],
-                "example1": [utterance1],
-                "name2": [intent2],
-                "example2": [utterance2],
-                "similarity": [score],
-            }
-        )
-        logger.info('Index done %s', index)
-        return temp_pd
-    return None
 
 def _calculate_cosine_similarity(logger, workspace_bow):
     """
