ab98d19e6c503510431917b74158ab747b3f7d8d
Paul Proell <paul.proell@hotmail.com>
2022-11-03 15:58:40 +0100

    added redis client timeout

--- api/redis_client.py
+++ api/redis_client.py
@@ -2,7 +2,7 @@ import os
 import redis
 
 
-def getRedis():
+def getRedis(timeout=600):
     redis_host = os.environ.get('REDIS_HOST', 'localhost')
     redis_port = int(os.environ.get('REDIS_PORT', 6379))
     redis_db = int(os.environ.get('REDIS_DB', 0))
@@ -12,8 +12,8 @@ def getRedis():
     redis_sentinel_name = os.environ.get('REDIS_SENTINEL_NAME', None)
     if redis_sentinel_host is not None:
         sentinel = redis.Sentinel(sentinels=[(redis_sentinel_host, redis_sentinel_port)],
-                                  db=redis_db, password=redis_password)
-        return sentinel.master_for(redis_sentinel_name)
+                                  db=redis_db, password=redis_password, socket_timeout=timeout)
+        return sentinel.master_for(redis_sentinel_name, socket_timeout=timeout)
     else:
         return redis.Redis(host=redis_host,
-                           port=redis_port, db=redis_db, password=redis_password)
+                           port=redis_port, db=redis_db, password=redis_password, socket_timeout=timeout)
