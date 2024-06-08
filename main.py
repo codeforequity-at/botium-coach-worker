38c3d69c1f23cae5e6d5be500bab9617c13e4520
Paul Proell <paul.proell@hotmail.com>
2024-06-08 23:19:26 +0200

    datetime import

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import numpy as np
 from api.utils.log import getLogger
 import psutil
 import copy
+from datetime import datetime
 
 def killtree(pid, including_parent=True):
     parent = psutil.Process(pid)
