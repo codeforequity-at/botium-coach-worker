bc172ba9de36701bcbdfd8b0426848c7ed93aa73
Paul Proell <paul.proell@hotmail.com>
2024-06-09 08:15:41 +0200

    disable status worker

--- api/embeddings.py
+++ api/embeddings.py
@@ -149,7 +149,7 @@ def calculate_embeddings_worker(logger, worker_name, req_queue, res_queue, err_q
         #status_queue.put(status_data)
 
     pstatus = mp.Process(target=status_update_worker, name='status_update_worker', args=(logger, log_extras, status_queue, res_queue))
-    pstatus.start()
+    #pstatus.start()
 
     #def kill_processes():
     #    logger.info('Killing status update worker', extra=log_extras)
